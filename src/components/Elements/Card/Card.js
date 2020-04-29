bce6150b454040fab1454da11068a95e963c5934
rosborne132 <ozborne132@gmail.com>
2020-04-28 23:20:11 -0700

    Update modal

--- src/components/Elements/Card/Card.js
+++ src/components/Elements/Card/Card.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Icon } from 'styledoui'
+import { motion } from 'framer-motion'
 
 import styles from './Card.module.css'
 
 export const Card = React.memo(({ icon, text }) => (
-    <div className={styles.body} key={icon}>
+    <motion.div className={styles.body} key={icon} whileHover={{ scale: 1.08 }}>
         <p>{text}</p>
         <Icon icon={icon} aria-hidden size="l3" />
-    </div>
+    </motion.div>
 ))
--- src/components/Elements/Modal/Modal.js
+++ src/components/Elements/Modal/Modal.js
@@ -1,30 +1,53 @@
 import React from 'react'
 import { Icon } from 'styledoui'
+import { motion, AnimatePresence } from 'framer-motion'
 
 import styles from './Modal.module.css'
 
 export const Modal = React.memo(({ children, isShowing, onClose }) => {
-    const transform = isShowing ? 'translateY(0vh)' : 'translateY(-100vh)'
-    const opacity = isShowing ? '1' : '0'
+    const variants = {
+        open: { y: 0 },
+        closed: { y: 50 }
+    }
 
     return (
-        isShowing && (
-            <div className={styles.container}>
-                <div className={styles.wrapper} style={{ transform, opacity }}>
-                    <header className={styles.header}>
-                        <span
-                            className={styles.closeBtn}
-                            onClick={onClose}
-                            onKeyDown={onClose}
-                            role="button"
-                            tabIndex={0}
-                        >
-                            <Icon icon="closeAlt" size="m1" aria-hidden />
-                        </span>
-                    </header>
-                    <div className={styles.body}>{children}</div>
-                </div>
-            </div>
-        )
+        <AnimatePresence>
+            {isShowing && (
+                <motion.div
+                    key="modal"
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    style={{
+                        position: 'fixed',
+                        top: '40%',
+                        left: '50%',
+                        transform: 'translate3d(-50%, -40%, 0)',
+                        zIndex: 3
+                    }}
+                >
+                    <motion.div
+                        className={styles.wrapper}
+                        variants={variants}
+                        initial="closed"
+                        animate="open"
+                        exit="closed"
+                    >
+                        <header className={styles.header}>
+                            <span
+                                className={styles.closeBtn}
+                                onClick={onClose}
+                                onKeyDown={onClose}
+                                role="button"
+                                tabIndex={0}
+                            >
+                                <Icon icon="closeAlt" size="m1" aria-hidden />
+                            </span>
+                        </header>
+                        <div className={styles.body}>{children}</div>
+                    </motion.div>
+                </motion.div>
+            )}
+        </AnimatePresence>
     )
 })
